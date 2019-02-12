c5753e4f23df7704696c0e2166a2f6b4473f9cb8
gopal10 <gopalpanchal10@gmail.com>
2019-02-13 00:33:23 +0530

    Update Prob4.java

--- src/main/java/com/stackroute/junit/Prob4.java
+++ src/main/java/com/stackroute/junit/Prob4.java
@@ -4,11 +4,10 @@ public class Prob4 {
 
     public String dis(int num) {
 
-        char a[]= new char[20];
         String str="";
-        for (int i = 1; i <= a.length; i++) {
-            for (int j = 1; j <= num; j++) {
-                str=str+a[i];
+        for (int i=1; i<=num; i++) {
+            for (int j=1; j<=i; j++) {
+                str=str+i;
             }
         }
            return str;
